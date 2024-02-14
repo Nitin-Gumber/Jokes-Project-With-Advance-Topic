4b0c2d5ef1f924bd2df85321715757e5fcb98960
Nitin Gumber <aroranitin486@gmail.com>
2024-02-14 16:48:16 +0530

    add: meta tags for seo remove: comments

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,8 @@
-
-// PROMISE :- 
-
-    // 2 condition ==> successfull ==> fail
-
-    // fetch(https:restcountries.eu/rest/v2/name/nepal)
-
-    // data milega ==> sab kuch shi h
-    // ya data nhi milega ==> internet error, ya server error
-
-    // fullfilled
-    // reject
-
-    // 90% promise comsumed krte h na ki create
-
+    "use strick"
+    
     const joke = document.querySelector("#joke")
     const jokeBtn = document.querySelector("#jokeBtn")
-
-
-    // const generateJokes = () => {
-
-    //     const setHeader = {
-    //         headers : {
-    //             Accept : "application/json"
-    //         }
-    //     }
-
-    //     fetch('https://icanhazdadjoke.com', setHeader)
-    //     .then((res) => res.json())
-    //     .then((data) =>
-    //         joke.innerHTML = data.joke
-    //     ).catch((error) => 
-    //         console.log(error)
-    //     )
-    // }
-
-
-    // Convert into Async- await with data Handling :- 
-
+ 
     const generateJokes = async () => {
 
         try{
